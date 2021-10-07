11fde2c86f7526d8baae11f5584f9a79eba5c52d
高橋　優稀 <iwanttoeatfish@gmail.com>
2021-10-07 17:23:56 +0900

    テスト

---
+++ nannkairoiro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GithubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Praise(s1);
+            Console.WriteLine(s2);
+        }
+        static string Praise(string s)
+        {
+           s=s+"さんやりますねぇ！";
+            return s;
+        }
+    }
+}
+
